ed03e1124238d4a89d40ea9f61be8566722effd3
harikiran138 <155863015+harikiran138@users.noreply.github.com>
2024-02-15 06:38:52 +0530

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,17 @@
+
+  var randomnumber1=Math.floor(Math.random()*6)+1;
+  var randomdice1="./images/dice"+randomnumber1+".png";
+  document.querySelectorAll("img")[0].setAttribute("src",randomdice1);
+
+  var randomnumber2=Math.floor(Math.random()*6)+1;
+  var randomdice2="./images/dice"+randomnumber2+".png";
+  document.querySelectorAll("img")[1].setAttribute("src",randomdice2);
+if(randomnumber1>randomnumber2){
+    document.querySelector("h1").innerHTML="player1 wins";
+}
+else if(randomnumber2>randomnumber1){
+document.querySelector("h1").innerHTML="player2 wins";
+}
+else{
+    document.querySelector("h1").innerHTML="Draw!";
+}
